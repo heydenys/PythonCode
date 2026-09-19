24f790e6d18bf2740d542aeb5486b02aece363c6
Denys <denyskuba@pm.me>
2026-09-19 05:35:45 +0000

    Updated a few lines.

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,4 +1,7 @@
+joe =  xyz
+Banana = 5+1038482+28883-123
 
 print("Hello, world!")
-
-print("This is a simple Python script... cool stuff.")
+print(joe)
+print("This is a simple Python script... cool stuff. Try typing joe or adding Banana to see what happens!")
+input("Press Enter to exit...")
